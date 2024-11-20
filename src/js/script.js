2c378ecf90d5eb4821763a00282a02b58f57e72b
Lippi-Ugo <ugo.lippi@edu.univ-fcomte.fr>
2024-11-20 14:45:19 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -6,14 +6,13 @@ const nextButton = document.querySelector(".carousel__button--next");
 // Largeur de défilement d’un item
 if (carousel) {
   // Scroll au clic sur le bouton précédent
-  prevButton.____________("_____", () => {
-    ______________________;
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -260, behavior: "smooth" });
   });
 
   // Scroll au clic sur le bouton suivant
-  nextButton.____________("_____", () => {
-    ______________________;
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: +260, behavior: "smooth" });
   });
-}
 }
 // TODO
